345d46b74d3d524025bdea8151702baab0e590c6
Peter Bloxidge <peter.bloxidge@virgin.net>
2021-05-22 13:50:42 +0100

    Initial commit

---
+++ Fruitful/Modules/FruitList/View/FruitListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FruitListViewController.swift
+//  Fruitful
+//
+//  Created by Peter Bloxidge on 22/05/2021.
+//
+
+import UIKit
+
+class FruitListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
---
+++ Fruitful/Supporting Files/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Fruitful
+//
+//  Created by Peter Bloxidge on 22/05/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
