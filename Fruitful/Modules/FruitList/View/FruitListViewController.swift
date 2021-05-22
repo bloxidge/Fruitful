c4cf8ece1a17ba2626aca4bdd54f9dc58b22f87f
Peter Bloxidge <peter.bloxidge@virgin.net>
2021-05-22 16:33:57 +0100

    Fix cell size for portrait/landscape orientation

--- Fruitful/Modules/FruitList/View/FruitListViewController.swift
+++ Fruitful/Modules/FruitList/View/FruitListViewController.swift
@@ -9,12 +9,14 @@ import UIKit
 
 class FruitListViewController: UIViewController {
     
+    struct Constants {
+        static let cellSpacing: CGFloat = 16
+    }
+    
     var presenter: FruitListPresenter!
     
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    
-    private let cellReuseIdentifier = "FruitListCollectionViewCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +24,12 @@ class FruitListViewController: UIViewController {
         setUpCollectionView()
         
         presenter.reload()
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        collectionView.collectionViewLayout.invalidateLayout()
     }
     
     private func setUpCollectionView() {
@@ -60,7 +68,8 @@ extension FruitListViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellReuseIdentifier, for: indexPath) as! FruitListCollectionViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FruitListCollectionViewCell",
+                                                      for: indexPath) as! FruitListCollectionViewCell
         let fruit = presenter.getFruit(at: indexPath.item)
         cell.title = fruit?.type
         return cell
@@ -71,6 +80,30 @@ extension FruitListViewController: UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
+    }
+}
+
+extension FruitListViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let isPortrait = collectionView.bounds.width < collectionView.bounds.height
+        let cellsPerRow: CGFloat = isPortrait ? 2 : 4
+        let width = (collectionView.bounds.width - (Constants.cellSpacing * (cellsPerRow - 1))) / cellsPerRow
+        return CGSize(width: width, height: width * 0.75)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return Constants.cellSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return Constants.cellSpacing
     }
 }
 
