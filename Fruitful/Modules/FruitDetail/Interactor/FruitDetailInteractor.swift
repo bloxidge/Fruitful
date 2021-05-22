8980f48b8d773133d4d2f4a1fd338976b4d1648b
Peter Bloxidge <peter.bloxidge@virgin.net>
2021-05-22 18:17:41 +0100

    Add module for FruitDetail screen

---
+++ Fruitful/Modules/FruitDetail/Interactor/FruitDetailInteractor.swift
@@ -0,0 +1,13 @@
+//
+//  FruitDetailInteractor.swift
+//  Fruitful
+//
+//  Created by Peter Bloxidge on 22/05/2021.
+//
+
+import Foundation
+import PromiseKit
+
+protocol FruitDetailInteractor: AnyObject {}
+
+class FruitDetailInteractorImpl: FruitDetailInteractor {}
---
+++ Fruitful/Modules/FruitDetail/View/FruitDetailView.swift
@@ -0,0 +1,12 @@
+//
+//  FruitDetailView.swift
+//  Fruitful
+//
+//  Created by Peter Bloxidge on 22/05/2021.
+//
+
+import Foundation
+
+protocol FruitDetailView {
+    func showDetails(for fruit: Fruit)
+}
